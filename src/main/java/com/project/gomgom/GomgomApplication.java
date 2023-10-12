84acc3560adccc5389a82e8db7cfe35c2f0b3741
MinseoKangQ <kms02171@naver.com>
2023-10-12 20:28:36 +0900

    Project init, Add swagger settings in build.gradle

---
+++ src/main/java/com/project/gomgom/GomgomApplication.java
@@ -0,0 +1,13 @@
+package com.project.gomgom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomgomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomgomApplication.class, args);
+	}
+
+}
