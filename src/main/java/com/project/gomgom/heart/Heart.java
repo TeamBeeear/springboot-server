66ad7bb24c3985047685cca26d944a6da771346d
MinseoKangQ <kms02171@naver.com>
2023-10-18 13:28:19 +0900

    Heart 엔티티 작성

---
+++ src/main/java/com/project/gomgom/heart/Heart.java
@@ -0,0 +1,27 @@
+package com.project.gomgom.heart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "HEART")
+public class Heart {
+
+    @Id @Column(name = "heart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long heartId;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "post_id")
+    private Long postId;
+}
