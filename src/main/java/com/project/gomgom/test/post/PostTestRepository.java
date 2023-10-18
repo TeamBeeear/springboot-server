7f5c90076b377e94fd9ccc84d5dda1f7bf238ead
MinseoKangQ <kms02171@naver.com>
2023-10-18 13:01:16 +0900

    post 테스트 주석

--- src/main/java/com/project/gomgom/test/post/PostTestRepository.java
+++ src/main/java/com/project/gomgom/test/post/PostTestRepository.java
@@ -1,9 +1,9 @@
-package com.project.gomgom.test.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostTestRepository extends JpaRepository<PostTestEntity, Long> {
-    PostTestEntity findPostTestEntityById(Long id);
-}
+//package com.project.gomgom.test.post;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface PostTestRepository extends JpaRepository<PostTestEntity, Long> {
+//    PostTestEntity findPostTestEntityById(Long id);
+//}
--- src/main/java/com/project/gomgom/test/post/PostTestService.java
+++ src/main/java/com/project/gomgom/test/post/PostTestService.java
@@ -1,24 +1,24 @@
-package com.project.gomgom.test.post;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class PostTestService {
-
-    private final PostTestRepository postTestRepository;
-
-
-    public PostTestEntity createPostTest(PostTestDto dto) {
-        PostTestEntity newEntity = new PostTestEntity();
-        newEntity.setTitle(dto.getTitle());
-        newEntity.setContent(dto.getContent());
-        PostTestEntity result = postTestRepository.save(newEntity);
-        return newEntity;
-    }
-
-    public PostTestEntity readPostTest(Long id) {
-        return postTestRepository.findPostTestEntityById(id);
-    }
-}
+//package com.project.gomgom.test.post;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class PostTestService {
+//
+//    private final PostTestRepository postTestRepository;
+//
+//
+//    public PostTestEntity createPostTest(PostTestDto dto) {
+//        PostTestEntity newEntity = new PostTestEntity();
+//        newEntity.setTitle(dto.getTitle());
+//        newEntity.setContent(dto.getContent());
+//        PostTestEntity result = postTestRepository.save(newEntity);
+//        return newEntity;
+//    }
+//
+//    public PostTestEntity readPostTest(Long id) {
+//        return postTestRepository.findPostTestEntityById(id);
+//    }
+//}
