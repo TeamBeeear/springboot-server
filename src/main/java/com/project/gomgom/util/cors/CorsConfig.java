33ba3edf38344a0d74c4ace224cf584e9edb1621
MinseoKangQ <kms02171@naver.com>
2023-11-27 16:44:20 +0900

    Feat: Edit cors, security setting

--- src/main/java/com/project/gomgom/util/cors/CorsConfig.java
+++ src/main/java/com/project/gomgom/util/cors/CorsConfig.java
@@ -1,17 +1,18 @@
 package com.project.gomgom.util.cors;
 
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // "*" 대신 allowedOriginPatterns 사용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
---
+++ src/main/java/com/project/gomgom/util/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.project.gomgom.util.security;
+
+// HTTP to HTTPS config
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().configurationSource(request -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOriginPatterns(List.of("*"));
+            cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+        });
+        http.authorizeRequests().anyRequest().permitAll();
+        return http.build();
+    }
+
+}
